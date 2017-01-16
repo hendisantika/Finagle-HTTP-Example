c0356c2d8c083065c77e11787151efec5fa8f40e
hendisantika <hendisantika@gmail.com>
2017-01-16 10:42:35 +0700

    Finagle-HTTP-Example

--- src/main/scala/finagle/Client3.scala
+++ src/main/scala/finagle/Client3.scala
@@ -2,19 +2,18 @@ package finagle
 
 import com.twitter.finagle.{Http, Service, http}
 import com.twitter.util.{Await, Future}
-import io.circe.parser.decode
-import cats.data
 
 /**
-  * Created by hendisantika on 28/12/16.
+  * Created by hendisantika on 16/01/17.
   * sbt 'run-main Client'
   *
   * GET success: Response("HTTP/1.1 Status(200)")
   */
-object Client2 extends App{
-  val client: Service[http.Request, http.Response] = Http.newService("http://10.2.15.5:8983/solr/interaction-repo/select?q=content_t%3Aibadah+%26%26%0Aarticle_id_l%3A6729941&wt=json&indent=true")
+object Client3 extends App{
+  val client: Service[http.Request, http.Response] = Http.newService("https://jsonplaceholder.typicode.com:80/posts/1")
   val request = http.Request(http.Method.Get, "/")
-  request.host = "http://10.2.15.5"
+//  request.host = "https://jsonplaceholder.typicode.com"
+  request.host = "104.31.87.157"
   val response: Future[http.Response] = client(request)
   Await.result(response.onSuccess { rep: http.Response =>
     println("GET success: " + rep)
