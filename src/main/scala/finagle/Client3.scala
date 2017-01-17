fd5aea1cb8b8e01f7c2e271d6d0a3690b9900315
hendisantika <hendisantika@gmail.com>
2017-01-17 18:56:00 +0700

    Fix Bug Client3

--- src/main/scala/finagle/Client3.scala
+++ src/main/scala/finagle/Client3.scala
@@ -9,20 +9,32 @@ import com.twitter.util.{Await, Future}
   *
   * GET success: Response("HTTP/1.1 Status(200)")
   */
-object Client3 extends App{
-  val client: Service[http.Request, http.Response] = Http.newService("https://jsonplaceholder.typicode.com:80/posts/1")
-  val request = http.Request(http.Method.Get, "/")
-//  request.host = "https://jsonplaceholder.typicode.com"
-  request.host = "104.31.87.157"
+object Client3 extends App {
+  val client: Service[http.Request, http.Response] = Http.newService("https://jsonplaceholder.typicode.com:80")
+  val request = http.Request(http.Method.Get, "/posts")
+  request.host = "https://104.31.86.157"
   val response: Future[http.Response] = client(request)
-  Await.result(response.onSuccess { rep: http.Response =>
-    println("GET success: " + rep)
+    Await.result(response.onSuccess { rep: http.Response =>
+      println("GET success: " + rep)
 
-//    val obj = decode[SolrDoc](rep.contentString)
-//    val solrDocument = obj match {
-//      case Xor.Right(r) => r
-//      case Xor.Left(l) => "document not found"
+  //    val obj = decode[SolrDoc](rep.contentString)
+  //    val solrDocument = obj match {
+  //      case Xor.Right(r) => r
+  //      case Xor.Left(l) => "document not found"
+  //    }
+
+    })
+//  val hit = Future {
+//    response.onSuccess { rep: http.Response =>
+//      println("GET success: " + rep)
+//      println("GET Content: " + rep.contentString)
+//
 //    }
+//  }
 
-  })
+//  hit onSuccess (_ => println("content --> " + rep ))
+
 }
+
+
+
